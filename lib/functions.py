ca6f2c16ac321b5fb5defc1acb0e5c26f0731a01
Blaque <blaque.develop@gmail.com>
2023-08-15 15:50:42 +0300

    Done with assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
+def my_function(param):
+    print("Running my_function")
+    return param + 1
+
+my_function_return_value = my_function(1)
+print(my_function_return_value)
+
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    sum = num1 + num2
+    return sum
 
 def halve(number):
-    pass
+    half = number * 0.5
+    return half
